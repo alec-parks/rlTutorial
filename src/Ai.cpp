604778223ed8bbc155e608bb08d3eae8aa87cfc1
Alexander Parks <alec.parks@gmail.com>
2015-10-27 08:29:05 -0500

    Added ability to drop items.

--- src/Ai.cpp
+++ src/Ai.cpp
@@ -117,6 +117,15 @@ void PlayerAi::handleActionKey(Actor *owner, int ascii ){
 	}
       }
     break;
+    case 'd' : //drop item
+    {
+      Actor *actor=chooseFromInventory(owner);
+      if( actor ) {
+        actor->pickable->drop(actor,owner);
+	engine.gameStatus=Engine::NEW_TURN;
+      }
+    }
+    break;
   }
 }
 
--- src/Pickable.cpp
+++ src/Pickable.cpp
@@ -17,6 +17,17 @@ bool Pickable::use(Actor *owner, Actor *wearer){
 	return false;
 }
 
+void Pickable::drop(Actor *owner, Actor *wearer) {
+  if (wearer->container) {
+    wearer->container->remove(owner);
+    engine.actors.push(owner);
+    owner->x=wearer->x;
+    owner->y=wearer->y;
+    engine.gui->message(TCODColor::lightGrey,"%s drops a %s.",
+		    wearer->name,owner->name);
+  }
+}
+
 Healer::Healer(float amount): amount(amount){
 }
 
